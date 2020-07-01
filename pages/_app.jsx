f7a22d3ee1c70823deeec7115543fd1122b234f8
Vinicius Audibert <vinicius.audibert-ext@viavarejo.com.br>
2020-07-01 19:11:30 -0300

    fix scripts

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -13,20 +13,19 @@ export default ({ Component, pageProps }) => (
     <Head>
       <script
         dangerouslySetInnerHTML={{
-          __html: `<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WR2BCBD');</script>
-<!-- End Google Tag Manager -->`,
+})(window,document,'script','dataLayer','GTM-WR2BCBD');`,
         }}
       />
       <style dangerouslySetInnerHTML={{ __html: `.async-hide { opacity: 0 !important}` }} />
       <script
         dangerouslySetInnerHTML={{
-          __html: `(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
-h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+          __html: `
+          (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
 (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
 })(window,document.documentElement,'async-hide','dataLayer',4000,
 {'GTM-WR2BCBD':true});`,
@@ -36,10 +35,10 @@ h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
 
     <noscript
       dangerouslySetInnerHTML={{
-        __html: `<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WR2BCBD"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->`,
+        __html: `
+<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WR2BCBD"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+`,
       }}
     />
 
