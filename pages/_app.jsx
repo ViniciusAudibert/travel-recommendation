439f9d0e43eee1d988d1fd50fe196db1ec417f3a
Vinicius Audibert <vinicius.audibert-ext@viavarejo.com.br>
2020-07-01 19:03:27 -0300

    add tracker

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -29,7 +29,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
 (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
 })(window,document.documentElement,'async-hide','dataLayer',4000,
-{'CONTAINER_ID':true});`,
+{'GTM-WR2BCBD':true});`,
         }}
       />
     </Head>
